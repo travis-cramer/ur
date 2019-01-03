bc1cff820641980d443cfa6455639abac34f0d06
travis-cramer <travis@ridecell.com>
2019-01-03 13:45:50 -0800

    add completing a piece functionality

--- play.py
+++ play.py
@@ -65,11 +65,13 @@ class Ur(object):
 		if move != "0" and self.board[move]["current_piece"] != self.board["current_turn"]:
 			if verbose: print("Invalid move. You do not have a piece there.")
 			return False
-		appropriate_path = self.board["{}_path".format(self.board["current_turn"]).lower()]  # either 'white_path' or 'black_path'
-		if (appropriate_path.index(move) + self.board["roll"]) > len(appropriate_path):
+		# either 'white_path' or 'black_path'
+		appropriate_path = self.board["{}_path".format(self.board["current_turn"]).lower()]
+		if (appropriate_path.index(move) + self.board["roll"]) > (len(appropriate_path) - 1):
 			if verbose: print("Invalid move. You must roll the exact number of squares to get off the board.")
 			return False
-		if self.board[self.get_new_position(move)]["current_piece"] == self.board["current_turn"]:
+		new_position = self.get_new_position(move)
+		if new_position != "21" and self.board[new_position]["current_piece"] == self.board["current_turn"]:
 			if verbose: print("Invalid move. You would land on your own piece.")
 			return False
 		return True
@@ -85,7 +87,9 @@ class Ur(object):
 			print("Overtook a white piece!")
 
 	def get_new_position(self, move):
-		appropriate_path = self.board["{}_path".format(self.board["current_turn"]).lower()]  # either 'white_path' or 'black_path'
+		# either 'white_path' or 'black_path'
+		current_color = self.board["current_turn"].lower()
+		appropriate_path = self.board["{}_path".format(current_color)]
 		return str(appropriate_path[appropriate_path.index(move) + self.board["roll"]])
 
 	def can_move(self):
@@ -97,21 +101,34 @@ class Ur(object):
 				a_valid_move_exists = True
 		return a_valid_move_exists
 
+	def complete_piece(self):
+		current_color = self.board["current_turn"].lower()
+		self.board["completed_{}".format(current_color)] += 1
+		if self.board["current_turn"] == "WHITE":
+			print("White completed a piece and scored a point!")
+		else:
+			print("Black completed a piece and scored a point!")
+
 	def play_move(self, move):
 		if move in ["quit", "gameover", "game over"]:
 			self.gameover = True
 			return True
 
 		if move == "0":
-			self.board["available_{}".format(self.board["current_turn"]).lower()] -= 1  # subtract one from either available_black or available_white
+			# subtract one from either available_black or available_white
+			self.board["available_{}".format(self.board["current_turn"]).lower()] -= 1
 		else:
 			self.board[move]["current_piece"] = None  # remove piece from old space
 
 		new_position = self.get_new_position(move)
 
-		if self.board[new_position]["current_piece"] and self.board[new_position]["current_piece"] != self.board["current_turn"]:
-			self.overtake_opposing_piece()
-		self.board[new_position]["current_piece"] = self.board["current_turn"]
+		if new_position == "21":
+			self.complete_piece()
+			return False  # return True so player can roll again, False to change turn
+		else:
+			if self.board[new_position]["current_piece"] in ["WHITE", "BLACK"]:
+				self.overtake_opposing_piece()
+			self.board[new_position]["current_piece"] = self.board["current_turn"]
 
 		return self.board[new_position]["is_rosette"]  # return True so player can roll again, False to change turn
 
