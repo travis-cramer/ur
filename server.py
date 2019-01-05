f3597782985e99c221e644c3ed495e03d0cd8fd3
travis-cramer <travis.c.cramer@gmail.com>
2019-01-05 01:44:35 -0800

    remove /test endpoint and default to board page

--- server.py
+++ server.py
@@ -7,10 +7,6 @@ app = Flask(__name__, static_folder='')
 
 @app.route("/")
 def games():
-	return "<a href='/games/1'>game 1</a><p></p><a href='/games/2'>game 2<a>"
-
-@app.route("/test/")
-def test():
 	return "<a href='/board?game_id=1'>game 1</a><p></p><a href='/board?game_id=2'>game 2<a>"
 
 @app.route("/board/")
